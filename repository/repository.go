4424b6b266f4673425b91fd717c003a5a26e93ea
mavolin <github@mavolin.co>
2022-11-06 15:23:50 +0100

    feat(repository): add database abstraction

---
+++ repository/repository.go
@@ -0,0 +1,77 @@
+package repository
+
+import (
+	"context"
+	"strconv"
+
+	"github.com/mavolin/sueno-dict/pkl/sueno"
+)
+
+type Repository interface {
+	// WordRoot returns the words associated with the given root, sorted
+	// by [Word.Type] and then by name.
+	WordRoot(ctx context.Context, root string) (*WordRoot, error)
+
+	CreateWord(context.Context, Word) (WordID, error)
+	Word(context.Context, WordID) (*Word, error)
+	SearchWord(context.Context, string) (*Word, error)
+}
+
+// ============================================================================
+// WordRoot
+// ======================================================================================
+
+type WordRoot struct {
+	Root  string
+	Words []Word
+}
+
+// ============================================================================
+// Word
+// ======================================================================================
+
+type WordID uint64
+
+func ParseWordID(s string) (WordID, error) {
+	idInt, err := strconv.ParseInt(s, 10, 64)
+	if err != nil {
+		return 0, err
+	}
+
+	return WordID(idInt), nil
+}
+
+type Word struct {
+	ID   WordID `gorm:"primary_key"`
+	Word string
+	Root string
+
+	Definitions   []Definition
+	CompoundWords []Word
+}
+
+// ============================================================================
+// Definition
+// ======================================================================================
+
+type DefinitionID uint64
+
+func ParseDefinitionID(s string) (DefinitionID, error) {
+	idInt, err := strconv.ParseInt(s, 10, 64)
+	if err != nil {
+		return 0, err
+	}
+
+	return DefinitionID(idInt), nil
+}
+
+type Definition struct {
+	ID     DefinitionID `gorm:"primary_key"`
+	WordID WordID
+	Type   sueno.WordType
+
+	Translation string
+
+	Example            string
+	ExampleTranslation string
+}
