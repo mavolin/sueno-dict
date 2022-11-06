8ba70cd12859f6ecf86a0bba0b69aee0523e4935
mavolin <github@mavolin.co>
2022-11-06 18:28:17 +0100

    feat(cmd/dict): read config

--- cmd/dict/dict.go
+++ cmd/dict/dict.go
@@ -3,7 +3,9 @@ package main
 import (
 	"go.uber.org/zap"
 
+	"github.com/mavolin/sueno-dict/internal/config"
 	"github.com/mavolin/sueno-dict/internal/setup/logger"
+	"github.com/mavolin/sueno-dict/repository/postgres"
 )
 
 func main() {
@@ -19,5 +21,20 @@ func main() {
 }
 
 func run(log *zap.SugaredLogger) error {
+	c, err := config.Read()
+	if err != nil {
+		return err
+	}
+
+	repo, err := postgres.Open(postgres.Options{
+		DBName:   c.DB.Name,
+		Host:     c.DB.Host,
+		User:     c.DB.User,
+		Password: c.DB.Password,
+	})
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -8,11 +8,14 @@ import (
 type Config struct {
 	Server struct {
 		Port string `env:"PORT" envDefault:"8080"`
+
+		ProtectedUser     string `env:"PROTECTED_USER" envRequired:"true"`
+		ProtectedPassword string `env:"PROTECTED_PASSWORD" envRequired:"true"`
 	} `envPrefix:"SERVER_"`
 
 	DB struct {
 		Host     string `env:"HOST" envDefault:"localhost"`
-		Name     string `env:"NAME" envDefault:"pengpeng"`
+		Name     string `env:"NAME" envDefault:"sueno_dict"`
 		User     string `env:"USER" envDefault:"root"`
 		Password string `env:"PASSWD" envDefault:"root"`
 	} `envPrefix:"DB_"`
