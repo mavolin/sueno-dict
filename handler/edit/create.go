b6fd45bec7585cca76a36fbfae5578314d71cabe
mavolin <github@mavolin.co>
2022-11-09 18:32:41 +0100

    fix(root): fix only the first definition of a word getting added to the db

--- handler/edit/create.go
+++ handler/edit/create.go
@@ -22,20 +22,22 @@ func (h *Handler) Create(gctx *gin.Context) {
 	}
 }
 
-type CreateForm struct {
-	Word string `form:"word"`
-	Root string `form:"root"`
+type (
+	CreateForm struct {
+		Word string `form:"word"`
+		Root string `form:"root"`
 
-	CompoundWords []string `form:"compound_words"`
+		CompoundWords []string `form:"compound_words"`
 
-	Definitions []struct {
-		Type               sueno.WordType `form:"type"`
-		Translation        string         `form:"translation"`
-		Definition         string         `form:"definition"`
-		Example            string         `form:"example"`
-		ExampleTranslation string         `form:"example_translation"`
-	} `form:"definitions"`
-}
+		Definitions struct {
+			Type               []sueno.WordType `form:"type"`
+			Translation        []string         `form:"translation"`
+			Definition         []string         `form:"definition"`
+			Example            []string         `form:"example"`
+			ExampleTranslation []string         `form:"example_translation"`
+		} `form:"definitions"`
+	}
+)
 
 func (h *Handler) ProcessCreate(gctx *gin.Context) {
 	var f CreateForm
@@ -68,15 +70,36 @@ func (h *Handler) ProcessCreate(gctx *gin.Context) {
 		}
 	}
 
-	if len(f.Definitions) > 0 {
-		w.Definitions = make([]repository.Definition, len(f.Definitions))
-		for i, d := range f.Definitions {
-			w.Definitions[i] = repository.Definition{
-				Type:               d.Type,
-				Translation:        d.Translation,
-				Definition:         d.Definition,
-				Example:            d.Example,
-				ExampleTranslation: d.ExampleTranslation,
+	if len(f.Definitions.Type) > 0 {
+		w.Definitions = make([]repository.Definition, len(f.Definitions.Type))
+
+		// I couldn't find any lib that allowed using slices of structs as
+		// form values without having to used indexes in the input's name
+		// attribute.
+		// So I could either use something like jquery.repeater, or just do
+		// this, which should suffice and doesn't require me to bring jquery
+		// into this otherwise very simple project.
+		//
+		// I have a slight suspicion that no lib supports this because the
+		// order of items in form value arrays is not guaranteed, but I
+		// couldn't confirm this during my own testing on Chrome.
+		for i := 0; i < len(w.Definitions); i++ {
+			w.Definitions[i].Type = f.Definitions.Type[i]
+
+			if i < len(f.Definitions.Translation) {
+				w.Definitions[i].Translation = f.Definitions.Translation[i]
+			}
+
+			if i < len(f.Definitions.Translation) {
+				w.Definitions[i].Definition = f.Definitions.Definition[i]
+			}
+
+			if i < len(f.Definitions.Example) {
+				w.Definitions[i].Example = f.Definitions.Example[i]
+			}
+
+			if i < len(f.Definitions.ExampleTranslation) {
+				w.Definitions[i].ExampleTranslation = f.Definitions.ExampleTranslation[i]
 			}
 		}
 	}
